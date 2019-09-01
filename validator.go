47fa7ab1b68fa5ef425eb5f140a0dcb9c3abb58f
Charles Naut <cpn585@gmail.com>
2019-09-01 10:04:29 +0900

    Add passport struct

--- validator.go
+++ validator.go
@@ -1,6 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+type Passport struct {
+	Country         string
+	ExpirationYear  int
+	ExpirationMonth time.Month
+}
 
 func main() {
 	fmt.Println("Welcome to your passport validator")
