5988d7bc8c7954c19d66f311bc8fe99fb3ead3af
Charles Naut <cpn585@gmail.com>
2019-09-04 19:57:06 +0300

    Add Date struct and basic expiration date checking

--- validator.go
+++ validator.go
@@ -5,12 +5,39 @@ import (
 	"time"
 )
 
+type Date struct {
+	Year  int
+	Month time.Month
+	Day   int
+}
+
 type Passport struct {
-	Country         string
-	ExpirationYear  int
-	ExpirationMonth time.Month
+	Country        string
+	ExpirationDate Date
+}
+
+func isPassportValid(passport Passport, returnDate Date) bool {
+	if passport.Country != "US" {
+		return false
+	}
+
+	if passport.ExpirationDate.Year < returnDate.Year {
+		return false
+	}
+
+	// Passport expires in the same year as the return date
+	if passport.ExpirationDate.Year == returnDate.Year {
+		if passport.ExpirationDate.Month < returnDate.Month {
+			return false
+		}
+	}
+
+	return true
 }
 
 func main() {
 	fmt.Println("Welcome to your passport validator")
+	passport := Passport{Country: "US", ExpirationDate: Date{Year: 2020}}
+	date := Date{Year: 2021}
+	fmt.Println(isPassportValid(passport, date))
 }
